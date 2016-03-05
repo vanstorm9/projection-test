9473004a8f4cb0d491244b1bfb34833b0b81a36d
vanstorm9 <antlowhur@yahoo.com>
2016-03-05 16:48:58 -0500

    Added a rotation script

---
+++ hologram_test/Assets/Scripts/rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+    private int ang;
+
+	// Use this for initialization
+	void Start () {
+        ang = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Sets the transforms rotation to rotate 30 degrees around the y-axis
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.up);
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.left);
+        ang = ang + 1;
+        
+    }
+}
